c4daeef50e48e2e54d076acbb412272d60304e97
garamappa <wel2u@naver.com>
2025-01-11 17:11:08 +0900

    fixed rotate error

---
+++ origin.py
@@ -0,0 +1,56 @@
+def Insert_Pictures(prs, path, slideIndex, shName, columns, maxPicCnt, whiteRatio, 
+                        temp = False, rot = False, startIndex=1, except_list=None):
+    slide=prs.slides[slideIndex]
+    files = GetFilePath(directoryPath=path, max_files=maxPicCnt, startIndex=startIndex, except_list = except_list)
+    
+    # 그림파일 형식 변환
+    tempfiles = []
+    
+    if temp:
+        if not os.path.exists(path+r'\temp'):
+            os.makedirs(path+r'\temp')
+        
+        for i in range(len(files)):
+            originFile = r'{0}\{1}'.format(path, files[i])
+            tempFile = r'{0}\temp\temp_{1}'.format(path, files[i])
+            img = Image.open(originFile)
+            img.save(tempFile)
+            tempfiles.append(tempFile)
+            
+    else:
+        for i in range(len(files)):
+            originFile = r'{0}\{1}'.format(path, files[i])
+            tempfiles.append(originFile)
+    
+    # 그림 삽입
+    for shape in slide.shapes:
+        if shape.name == shName: # ppt 표 이름으로 shape 할당
+            tableleft = shape.left
+            tabletop = shape.top
+            tableWidth = shape.width
+            tableHeight = shape.height
+            
+            rows = maxPicCnt / columns
+            picWidth = (tableWidth * (1-whiteRatio)) / columns            
+            picHeight = (tableHeight * (1-whiteRatio)) / rows
+            
+            rowGap = (tableHeight - (picHeight*rows)) / (rows * 2)
+            colGap = (tableWidth - (picWidth*columns)) / (columns * 2)
+            
+            rowGap2 = (tableHeight - (picWidth*rows)) / rows # front 사진만 적용
+            
+            startleft = tableleft + (colGap if columns > 1 else colGap * 2 * (-1))
+            starttop = tabletop + (rowGap if columns > 1 else rowGap2 / 2)
+
+                
+            for i in range(len(tempfiles)):
+                row = i % columns
+                col = int(i / columns)
+                left = startleft + (((colGap*2) + picWidth) * row)   
+                top = starttop + (((rowGap*2) + picHeight) * col)                
+                
+                if rot:                    
+                    pic = slide.shapes.add_picture(tempfiles[i], left=left, top=top, width=picHeight, height=picWidth)
+                    pic.rotation = 90
+                else:
+                    pic = slide.shapes.add_picture(tempfiles[i], left=left, top=top, width=picWidth, height=picHeight)
